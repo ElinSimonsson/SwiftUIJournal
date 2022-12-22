57a38baa1efa44c5063a6a54004c71701645fc74
Elin <elin.simonsson@outlook.com>
2022-12-22 09:02:06 +0100

    Initial Commit

---
+++ SwiftUIJournal/SwiftUIJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIJournalApp.swift
+//  SwiftUIJournal
+//
+//  Created by Elin Simonsson on 2022-12-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
